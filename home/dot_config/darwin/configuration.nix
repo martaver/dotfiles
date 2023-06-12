8bb2b110387e6eafd50f2151266b4a0468a7e27c
Sebastian Nemeth <sebastian@cleric.sh>
2023-06-12 13:43:39 +0300

    fix syntax error in hom.nix

--- home/dot_config/darwin/configuration.nix
+++ home/dot_config/darwin/configuration.nix
@@ -56,7 +56,7 @@ in
   nix.extraOptions = ''
     extra-platforms = aarch64-darwin x86_64-darwin
     experimental-features = nix-command flakes
-    trusted-users = root martaver
+    trusted-users = root martaver sebastiannemeth
     allow-import-from-derivation = true
   '';
 
