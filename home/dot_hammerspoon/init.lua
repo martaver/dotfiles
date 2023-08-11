1f7bb9c1b888ec6b07c7a8395bbe5296f495480a
Sebastian Nemeth <sebastian@cleric.sh>
2023-08-11 22:56:48 +0300

    hs: only load EmmyLua annotations for dotfiles repo

--- home/dot_hammerspoon/init.lua
+++ home/dot_hammerspoon/init.lua
@@ -1,5 +1,7 @@
+-- EmmyLua generates completion files in "/Users/<user>/.hammerspoon/Spoons/EmmyLua.spoon/annotations"
 hs.loadSpoon("EmmyLua")
 
+-- Hello World
 hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "W", function()
   hs.alert.show("Hello World!")
 end)
