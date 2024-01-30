9e2b5ba3a9e1a66c9eb93da704e933f9beec2db8
Sebastian Nemeth <sebastian@cleric.sh>
2024-01-30 15:09:38 +0200

    add PHLTHEL0266s-MacBook-Pro as host

--- home/dot_config/darwin/flake.nix
+++ home/dot_config/darwin/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Architeuthis";
+  description = "Hosts";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
@@ -10,6 +10,21 @@
   };
 
   outputs = { self, nixpkgs, darwin, home-manager }: {
+    # PHLTHEL0266s-MacBook-Pro
+    darwinConfigurations."PHLTHEL0266s-MacBook-Pro" = darwin.lib.darwinSystem {      
+      system = "aarch64-darwin";  # Apple Silicon      
+      modules = [
+        ./configuration.nix                                
+        home-manager.darwinModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.sebastiannemeth = import ./home.nix;
+        }
+      ];
+    };
+
+    # "Architeuthis"
     darwinConfigurations."Architeuthis" = darwin.lib.darwinSystem {      
       system = "aarch64-darwin";  # Apple Silicon
       # system = "x86_64-darwin"; # Intel
