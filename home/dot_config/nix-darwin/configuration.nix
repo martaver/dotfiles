908d39a5e8c7b0e127e07ec563df1924b3db867d
Sebastian Nemeth <6123570+martaver@users.noreply.github.com>
2025-03-01 23:01:56 +0200

    Update configuration.nix
    Add missing default options from freshly initialised nix-darwin flake.

--- home/dot_config/nix-darwin/configuration.nix
+++ home/dot_config/nix-darwin/configuration.nix
@@ -5,8 +5,14 @@ let
 
 in
 {
+  # Disable nix-darwin's control of the nix installation, which is managed by Determinate.
   nix.enable = false;
 
+  # Set Git commit hash for darwin-version.
+  system.configurationRevision = self.rev or self.dirtyRev or null;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
   system.stateVersion = 6;
 
   # Default system configurations
