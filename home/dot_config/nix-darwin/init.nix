585a4bd2804ea4294b592677fdd95a09a936d273
Sebastian Nemeth <6123570+martaver@users.noreply.github.com>
2025-03-02 00:08:14 +0200

    Update init.nix

--- home/dot_config/nix-darwin/init.nix
+++ home/dot_config/nix-darwin/init.nix
@@ -1,1 +1,26 @@
-# test
+# Empty system configuration, used for initialising nix-darwin for the first time
+# This way, other configuration options won't impact the installation
+
+{ config, pkgs, ... }:
+
+# All system packages
+let
+
+in
+{
+  # Disable nix-darwin's control of the nix installation, which is managed by Determinate.
+  nix.enable = false;
+
+  # Set Git commit hash for darwin-version.
+  system.configurationRevision = self.rev or self.dirtyRev or null;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 6;
+
+  # System packages
+  environment.systemPackages = [
+    
+  ];
+
+}
