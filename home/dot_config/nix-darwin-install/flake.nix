a087d230c16cd96bffdfbf11ad2ce56be999522f
Sebastian Nemeth <6123570+martaver@users.noreply.github.com>
2025-03-02 00:27:11 +0200

    Create flake.nix

---
+++ home/dot_config/nix-darwin-install/flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Empty nix-darwin flake for installation of nix-darwin";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  let
+    configuration = { pkgs, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        [
+        ];
+
+      # Necessary for using flakes on this system.
+      nix.enable = false
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 6;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "x86_64-darwin";
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#init
+    darwinConfigurations."init" = nix-darwin.lib.darwinSystem {
+      modules = [ configuration ];
+    };
+  };
+}
